ccc79ff4c1b9022c765c205c2e83ecaa99294546
cwhitsell <49255787+cwhitsell@users.noreply.github.com>
2021-04-29 20:54:42 -0700

    Add files via upload

---
+++ src/main/java/csc174project/Flowershop/App.java
@@ -0,0 +1,29 @@
+package csc174project.Flowershop;
+
+import javax.swing.*;
+import java.awt.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 JFrame frame = new JFrame("Hello World Java Swing");
+
+         // set frame site
+         frame.setMinimumSize(new Dimension(800, 600));
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+         // center the JLabel
+         JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
+
+         // add JLabel to JFrame
+         frame.getContentPane().add(lblText);
+
+         // display it
+         frame.pack();
+         frame.setVisible(true);
+    }
+}
